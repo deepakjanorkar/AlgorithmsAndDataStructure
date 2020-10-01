fb4fe9a74617d6a9450bdf6460f070fbd9ec9800
deepakjanorkar <48157935+deepakjanorkar@users.noreply.github.com>
2020-10-01 17:00:35 +0530

    Update LinearSearch.py

--- Python/Algorithms/SearchingAlgorithms/LinearSearch.py
+++ Python/Algorithms/SearchingAlgorithms/LinearSearch.py
@@ -4,20 +4,15 @@
 # If x is present then return its location, 
 # otherwise return -1 
 
-def search(array, key): 
-	n = len(array)
-	for i in range (0, n): 
-		if (array[i] == key): 
-			return i; 
-	return -1; 
 
-# Driver Code 
-array = [ 2, 3, 4, 10, 40 ]; 
-x = 10; 
-result = search(array, x) 
-if(result == -1): 
-	print("Element is not present in array") 
-else: 
-	print("Element is present at index", result); 
+def linearsearch(arr, x):
+   for i in range(len(arr)):
+      if arr[i] == x:
+         return i
+   return -1
+arr = ['p','y','t','h','o','n']
+x = 'o'
+print("element found at index "+str(linearsearch(arr,x)))
 
-#output: Element is present at index 3
+
+#output: element found at index 4
